ac3da965b7888ba954ec54bda03f4a372270558d
Chloe-J1 <chloe.jonckheere@student.howest.be>
2026-03-31 21:28:54 +0200

    Tower

--- Assets/Scripts/Tower/TowerRanger.cs
+++ Assets/Scripts/Tower/TowerRanger.cs
@@ -1,16 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class TowerRanger : TowerBase
 {
     [SerializeField] private int _damage = 1;
+
     public override void Attack(Enemy enemy)
     {
         base.Attack(enemy);
-        Debug.Log(CanAttack);
         if(_canAttack)
         {
             enemy.Health -= _damage;
             _canAttack = false;
-        }
+        }       
     }
 }
