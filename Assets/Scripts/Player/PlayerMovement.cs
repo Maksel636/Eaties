b7cfb7d36d0b8f35f6019cbd97a4e989da27b5d3
Chloe-J1 <chloe.jonckheere@student.howest.be>
2026-03-31 16:17:44 +0200

    Player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private CharacterController _controller;
+    [SerializeField]
+    private InputActionReference _moveInput;
+    [SerializeField]
+    private float _moveSpeed = 3f;
+    [SerializeField]
+    private PlayerInput _playerInput;
 
-    // Update is called once per frame
+    private float _verticalSpeed = 0f;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
     void Update()
     {
-        
+        Vector2 input = _playerInput.actions["Move"].ReadValue<Vector2>(); ;
+        Vector3 moveVelocity = new Vector3(input.x, 0f, input.y) * _moveSpeed;
+        Debug.Log(input);
+        _controller.Move(moveVelocity * Time.deltaTime);
+
+        //apply gravity
+        _verticalSpeed += Physics.gravity.y * Time.fixedDeltaTime;
+        _controller.Move(Vector3.up * _verticalSpeed * Time.fixedDeltaTime);
+        if (_controller.isGrounded)
+        {
+            _verticalSpeed = 0f;
+        }
     }
 }
