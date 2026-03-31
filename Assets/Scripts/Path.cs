6d0282b072124f21e1d32ff48b2f5612f567e93b
GoeminneSenne <Senne.goeminne@student.howest.be>
2026-03-31 16:04:35 +0200

    Added Path class

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public static Path Instance { get; set; }
+
+    private Transform[] _waypoints;
+    public Transform[] Waypoints => _waypoints;
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Debug.LogError("Trying to create second instance of Singleton class");
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(this);
+
+        Debug.Log(transform.childCount);
+
+        _waypoints = new Transform[transform.childCount];
+        for (int idx = 0; idx < transform.childCount; ++idx)
+        {
+            _waypoints[idx] = transform.GetChild(idx);
+        }
+    }
+}
