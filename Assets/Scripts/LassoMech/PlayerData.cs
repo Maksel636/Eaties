7268bcc4a2204288d1fe557f9ea934a3df1d41dc
Maksel636 <axeldruyts@gmail.com>
2026-04-01 12:10:07 +0200

    added multiplayer lasso mech

---
+++ Assets/Scripts/LassoMech/PlayerData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.LassoMech
+{
+    [System.Serializable]
+    public class PlayerData
+    {
+        public Transform player;
+        public Transform indicatororigin;
+
+        public float rotation;
+        public int direction;
+        public float escapeSteps;
+    }
+}
+
