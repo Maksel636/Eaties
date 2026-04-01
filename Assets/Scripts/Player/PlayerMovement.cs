1b8c103a57ee030988ebd39fa1f0c3835fb18919
Chloe-J1 <chloe.jonckheere@student.howest.be>
2026-04-01 20:14:57 +0200

    Capture with lasso

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -49,6 +49,10 @@ public class PlayerMovement : MonoBehaviour
         {
             _verticalSpeed = 0f;
         }
+
+
+        // Capture enemies
+        HandleEnemyCapture();
     }
 
     void HandleGrabInput(InputAction.CallbackContext context)
@@ -81,22 +85,23 @@ public class PlayerMovement : MonoBehaviour
 
         
         // Pickup enemy
-        Collider[] hits = Physics.OverlapSphere(transform.position, _grabRadius, _enemyMask);
-        foreach (Collider hit in hits)
-        {
-            Enemy enemy = hit.GetComponent<Enemy>();
+        //Collider[] hits = Physics.OverlapSphere(transform.position, _grabRadius, _enemyMask);
+        //foreach (Collider hit in hits)
+        //{
+        //    Enemy enemy = hit.GetComponent<Enemy>();
 
-            EscapeAnimal escape = enemy.GetComponentInChildren<EscapeAnimal>();
-            if (escape == null || !escape.IsCaptured) // verify that the creature is captured, if not
-                continue; // skip this enemy
+        //    EscapeAnimal escape = enemy.GetComponentInChildren<EscapeAnimal>();
+        //    if (escape == null || !escape.IsCaptured) // verify that the creature is captured, if not
+        //        continue; // skip this enemy
 
+        //    Debug.LogError("Captures creature");
 
-            _currentEnemy = hit.GetComponent<Enemy>();
-            _currentEnemy.IsPickedUp = true;
-            _currentEnemy.transform.SetParent(_grabSocket);
-            _currentEnemy.transform.localPosition = Vector3.zero;
-            break;
-        }
+        //    _currentEnemy = escape.GetComponentInParent<Enemy>();
+        //    _currentEnemy.IsPickedUp = true;
+        //    _currentEnemy.transform.SetParent(_grabSocket);
+        //    _currentEnemy.transform.localPosition = Vector3.zero;
+        //    break;
+        //}
         
 
         if (_currentEnemy != null) return; // You just picked up an enemy
@@ -147,5 +152,21 @@ public class PlayerMovement : MonoBehaviour
         {
             Destroy(_currentMeat.gameObject);
         }
+    }
+
+    void HandleEnemyCapture()
+    {
+        foreach(var enemy in EnemyManager.Instance.Enemies)
+        {
+            EscapeAnimal escape = enemy.GetComponentInChildren<EscapeAnimal>();
+            if (escape == null || !escape.IsCaptured) // verify that the creature is captured, if not
+                return; // skip this enemy
+
+            _currentEnemy = enemy.GetComponent<Enemy>();
+            _currentEnemy.IsPickedUp = true;
+            _currentEnemy.transform.SetParent(_grabSocket);
+            _currentEnemy.transform.localPosition = Vector3.zero;
+        }
+        
     }
 }
