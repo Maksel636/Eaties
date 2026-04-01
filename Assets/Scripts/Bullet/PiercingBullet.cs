4afae822f14c2a7d717461c660e0e859a45216d2
GoeminneSenne <Senne.goeminne@student.howest.be>
2026-04-01 14:51:49 +0200

    Removed debug messages

--- Assets/Scripts/Bullet/PiercingBullet.cs
+++ Assets/Scripts/Bullet/PiercingBullet.cs
@@ -2,16 +2,12 @@ using UnityEngine;
 
 public class PiercingBullet : Bullet
 {
-    private int _piercedTargets = 0;
 
     protected override void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             other.GetComponent<Enemy>().TakeDamage(Damage);
-
-            _piercedTargets++;
-            Debug.Log(_piercedTargets);
         }
     }
 }
