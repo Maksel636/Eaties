3afee72c481ca08d6ca609999c184865562b9edf
GoeminneSenne <Senne.goeminne@student.howest.be>
2026-04-01 13:48:47 +0200

    Bullets predict Enemy trajectory

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -57,4 +57,9 @@ public class Enemy : MonoBehaviour
     {
         EnemyManager.Instance?.UnRegisterEnemy(this);
     }
+
+    public Vector3 GetPredictedPosition(float futureTime)
+    {
+        return _movement.GetPredictedPosition(futureTime);
+    }
 }
--- Assets/Scripts/Tower/TowerRanger.cs
+++ Assets/Scripts/Tower/TowerRanger.cs
@@ -1,10 +1,21 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TowerRanger : TowerBase
 {
     [SerializeField] private int _damage = 1;
     [SerializeField] private GameObject _bulletPrefab;
+
+    private float _bulletSpeed = 0;
+
+    private void Start()
+    {
+        _bulletSpeed = _bulletPrefab.GetComponent<Bullet>().FlightSpeed;
+
+        Debug.Log("Start");
+        Debug.Log(_bulletSpeed);
+    }
 
     public override void Attack(Enemy enemy)
     {
@@ -19,15 +30,22 @@ public class TowerRanger : TowerBase
             Vector3 start = transform.position;
             start.y += 0.6f;
 
+
             Vector3 end = enemy.transform.position;
+
+            float time = Vector3.Distance(start, end) / _bulletSpeed;
+
+            Vector3 futureEnd = enemy.GetPredictedPosition(time);
+
             float duration = 0.4f;
 
-            Debug.DrawLine(start, end, Color.red, duration);
+            Debug.DrawLine(start, end, Color.blue, duration);
+            Debug.DrawLine(start, futureEnd, Color.red, time);
 
             var bulletObj = Instantiate(_bulletPrefab, start, Quaternion.Euler(0,0,0));
             
             var bullet = bulletObj.GetComponent<Bullet>();
-            bullet.Direction = (end - start).normalized;
+            bullet.Direction = (futureEnd - start).normalized;
             bullet.Damage = _damage;
         }       
     }
